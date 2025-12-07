f1664c4dd722cf2c6a2ad89ad4e31a2ec103c968
choiyoungkwon12 <hpwh4457@naver.com>
2025-12-07 17:42:10 +0900

    13주차 문제 제출 - 최영권

---
+++ 2025-season6/13주차/최영권/Solution.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        String answer = "";
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for (String s : completion) {
+            map.put(s, map.getOrDefault(s, 0) - 1);
+        }
+        for (String s : map.keySet()) {
+            if (map.get(s) != 0) {
+                answer = s;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
